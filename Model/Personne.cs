90f4f258ea56c9ab453d7471496cf41c1fa81e78
munyeshaka <aimablemunyeshaka30@gmail.com>
2021-09-10 15:15:12 +0200

    model _personne and resident

---
+++ Model/Personne.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Personne
+    {
+        string nom, prenom, genre, etatCivil, lieuNaissance;
+
+        public string LieuNaissance
+        {
+            get { return lieuNaissance; }
+            set { lieuNaissance = value; }
+        }
+
+        public string EtatCivil
+        {
+            get { return etatCivil; }
+            set { etatCivil = value; }
+        }
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+    }
+}
---
+++ Model/Resident.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Resident: Personne
+    {
+        string cni, nationnalite, dateNaissance;
+
+        public string DateNaissance
+        {
+            get { return dateNaissance; }
+            set { dateNaissance = value; }
+        }
+
+        public string Nationnalite
+        {
+            get { return nationnalite; }
+            set { nationnalite = value; }
+        }
+
+        public string Cni
+        {
+            get { return cni; }
+            set { cni = value; }
+        }
+    }
+}
