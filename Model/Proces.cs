8dde32b33b1a6154b0e585434c5badf9d1af70f9
unknown <nihezagireyvan@gmail.com>
2021-09-21 04:51:10 +0200

    Proces Model created

---
+++ Model/Proces.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Proces
+    {
+        string id, nom, date, pv, province;
+
+       
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string Pv
+        {
+            get { return pv; }
+            set { pv = value; }
+        }
+       
+
+        Parcelle parcelle;
+
+        public Parcelle Parcelle
+        {
+            get { return parcelle; }
+            set { parcelle = value; }
+        }
+
+        public string Province
+        {
+            get { return parcelle.Province; }
+            set { province = value; }
+        }
+       
+    }
+}
